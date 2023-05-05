2a27c52c41f61222890dcd738e65a486b211f893
gfbritto <gfbritto@outlook.com>
2023-05-04 21:42:28 -0300

    feat: add get by id and update methods

--- src/controllers/booksController.js
+++ src/controllers/booksController.js
@@ -1,9 +1,23 @@
-import books from "../models/Book.js";
+import Book from "../models/Book.js";
 
 class LivroController {
+
+    static async getById(req, res) {
+        try {
+            const { id } = req.params;
+            const book = await Book.findById(id);
+            if (!book) {
+                return res.status(404).send({ message: 'Book not found' });
+            }
+            res.status(200).send(book);
+        } catch (error) {
+            return res.status(500).json({ error: error.message });
+        }
+    }
+    
     static async getAll(req, res) {
         try {
-            const response = await books.find();
+            const response = await Book.find();
             console.log(response);
             return res.send(response);
         } catch (error) {
@@ -13,12 +27,50 @@ class LivroController {
 
     static async create(req, res) {
         try {
-            const book = new book(req.body);
-            const createdBookResponse = await books.create(book);
+            const book = new Book(req.body);
+            const createdBookResponse = await book.create(book);
             res.status(201).send(createdBookResponse)
         } catch (error) {
             return res.status(500).json({ error: error.message });
         }
+    }
+
+    static async update(req, res) {
+        try {
+            const { id, title, author, publisher, numberOfPages } = req.body;
+            const bookToUpdate = { title, author, publisher, numberOfPages };
+            
+            const result = await Book.findByIdAndUpdate(
+                id,
+                bookToUpdate,
+                { new: true }
+            );
+            if (!result) {
+                return res.status(404).send({ message: 'Livro não encontrado' });
+            }
+            return res.status(200).send(result);
+        } catch (error) {
+            return res.status(500).json({ error: error.message });
+        }
+    }
+
+    static async delete(req, res) {
+        try {
+            const { id } = req.params
+            console.log(id);
+            const response = Book.deleteOne({ id: id });
+            console.log(response.deletedCount)
+            if (response.deletedCount === 0) {
+                res.status(400).send(`The book with id ${id} does not exist`);
+            }
+            return res.status(204).send(`The book with id ${id} has been deleted`);
+        } catch (error) {
+            return res.status(500).json({ error: error.message });
+        }
+    }
+
+    static findBookById(id) {
+        return Books.findIndex(book => book.id == id);
     }
 }
 
--- src/routes/booksRoutes.js
+++ src/routes/booksRoutes.js
@@ -5,6 +5,12 @@ const router = express.Router();
 
 router.get('/books', booksController.getAll);
 
+router.get('/books/:id', booksController.getById);
+
 router.post('/books', booksController.create);
+
+router.put('/books/', booksController.update);
+
+router.delete('/books/:id', booksController.delete);
 
 export default router
